5cbc549c4be92a72c82c3059bf0ff63d6578b0b6
Kuanyshbek <it13@kulanoil.kz>
2026-06-03 17:49:28 +0500

    feat: add loading skeleton for posts list

---
+++ components/Postskeleton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="border border-gray-100 rounded-xl p-6 animate-pulse">
+    <div class="flex gap-2 mb-3">
+      <div class="h-5 w-14 bg-gray-100 rounded-full" />
+      <div class="h-5 w-10 bg-gray-100 rounded-full" />
+    </div>
+    <div class="h-5 w-3/4 bg-gray-100 rounded mb-2" />
+    <div class="h-4 w-full bg-gray-100 rounded mb-1" />
+    <div class="h-4 w-2/3 bg-gray-100 rounded mb-4" />
+    <div class="flex gap-4">
+      <div class="h-3 w-20 bg-gray-100 rounded" />
+      <div class="h-3 w-14 bg-gray-100 rounded" />
+    </div>
+  </div>
+</template>
